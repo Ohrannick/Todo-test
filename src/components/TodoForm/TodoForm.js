28788750a91e3dc9e1fb6c5ad93059425b7f27ab
Ohrannick <76492189+Ohrannick@users.noreply.github.com>
2021-11-07 12:02:13 +0300

    end of work

--- src/components/TodoForm/TodoForm.js
+++ src/components/TodoForm/TodoForm.js
@@ -1,5 +1,57 @@
-function TodoForm() {
-  return <div>TodoForm</div>;
+import { useState } from "react";
+import s from "./TodoForm.module.css";
+
+// Компонента для ввода задачи
+function TodoForm({ todo, setTodo }) {
+  const [value, setValue] = useState("");
+  const [plholder, setPlholder] = useState("Введите наименование задачи...");
+
+  // Функция для добавления задачи в стейт
+  // при пустом сообщении ничего не добавляется
+  // при повторном вводе одинаковой задачи Placeholder меняется
+  function saveTodo() {
+    let repeatTodo = todo.some((item) => item.title === value);
+    if (repeatTodo) {
+      setPlholder("---- Введите уникальную задачу ----");
+    } else if (value && !repeatTodo) {
+      setTodo(
+        [
+          ...todo,
+          {
+            id: new Date().getTime(),
+            title: value,
+            date: new Date().toLocaleTimeString(),
+            status: true,
+          },
+        ],
+        setPlholder("Введите наименование задачи...")
+      );
+    }
+    setValue("");
+  }
+
+  // отрабатывает нажатие Enter
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      setValue(e.target.value);
+      saveTodo();
+    }
+  };
+
+  return (
+    <div className={s.inputForm}>
+      <input
+        className={s.inputTodo}
+        placeholder={plholder}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        onKeyDown={handleKeyPress}
+      />
+      <button className={s.btn} onClick={saveTodo}>
+        Сохранить
+      </button>
+    </div>
+  );
 }
 
 export default TodoForm;
--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,5 +1,112 @@
-function TodoList() {
-  return <div>Header</div>;
+import { useState } from "react";
+import s from "./TodoList.module.css";
+
+// Компонента для отображения списка задачи
+// при отсутствии задач - указывает на их отсутствие
+function TodoList({ todo, setTodo }) {
+  const [edit, setEdit] = useState(null);
+  const [value, setValue] = useState("");
+
+  // удаляет задачу из стейта
+  function deleteTodo(id) {
+    let newTodo = [...todo].filter((item) => item.id !== id);
+    setTodo(newTodo);
+  }
+
+  // меняет статус задачи при ее закрытии (кнопка Закрыть / Открыть)
+  // или при нажатии на наименовании задачи
+  function statusTodo(id) {
+    let newTodo = [...todo].filter((item) => {
+      if (item.id === id) {
+        item.status = !item.status;
+      }
+      return item;
+    });
+    setTodo(newTodo);
+  }
+
+  // редактирование наименования задачи
+  function editTodo(id, title) {
+    setEdit(id);
+    setValue(title);
+  }
+
+  // сохранение задачи после редактирования
+  function saveTodo(id) {
+    let newTodo = [...todo].map((item) => {
+      if (item.id === id) {
+        item.title = value;
+      }
+      return item;
+    });
+    setTodo(newTodo);
+    setEdit(null);
+  }
+
+  // отрабатывает нажатие Enter
+  const handleKeyPress = (e, id) => {
+    if (e.key === "Enter") {
+      setValue(e.target.value);
+      saveTodo(id);
+    }
+  };
+
+  return (
+    <div>
+      {todo.length === 0 ? (
+        <h3>Задачи отсутствуют</h3>
+      ) : (
+        todo.map((item) => (
+          <div className={s.items} key={item.id}>
+            {edit === item.id ? (
+              <div className={s.editTodo}>
+                <input
+                  className={s.inputTodo}
+                  onChange={(e) => setValue(e.target.value)}
+                  onKeyDown={(e) => handleKeyPress(e, item.id)}
+                  value={value}
+                />
+              </div>
+            ) : (
+              <div className={s.item} onClick={() => statusTodo(item.id)}>
+                <div className={!item.status ? s.active : ""}>{item.title}</div>
+                <div className={s.itemDate}>{item.date}</div>
+              </div>
+            )}
+
+            {edit === item.id ? (
+              <div>
+                <button
+                  className={`${s.btn} ${s.btnClose}`}
+                  onClick={() => saveTodo(item.id)}
+                >
+                  Сохранить
+                </button>
+              </div>
+            ) : (
+              <div>
+                <button className={s.btn} onClick={() => deleteTodo(item.id)}>
+                  Удалить
+                </button>
+                <button
+                  className={`${s.btn} ${s.btnEdit}`}
+                  onClick={() => editTodo(item.id, item.title)}
+                >
+                  Редактировать
+                </button>
+                <button
+                  className={`${s.btn} ${s.btnClose}`}
+                  onClick={() => statusTodo(item.id)}
+                >
+                  {item.status ? "Закрыть" : "Открыть"}
+                </button>
+              </div>
+            )}
+          </div>
+        ))
+      )}
+    </div>
+  );
 }
 
 export default TodoList;
